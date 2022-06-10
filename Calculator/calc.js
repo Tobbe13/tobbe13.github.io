5136022ee20cf06b5078de874c11d4ca43943910
Tobbe13 <107103954+Tobbe13@users.noreply.github.com>
2022-06-10 10:20:18 +0200

    added projects

---
+++ Calculator/calc.js
@@ -0,0 +1,27 @@
+const btns = document.querySelectorAll(".calcBtn")
+
+btns.forEach((node) => {
+    node.addEventListener("mousedown", function(event) {
+        event.preventDefault()
+        const value = node.innerText.trim()
+        const $screen = document.querySelector(".screen")
+        const resultText = $screen.innerText.trim()
+
+        if(resultText == "0" || resultText == "undefined" || resultText == "Infinity") {
+            $screen.innerText = ""
+        }
+
+        if (value == "=") {
+            let solution = eval(resultText)
+            $screen.innerText = solution
+            return true
+        }
+
+        if (value.toLowerCase() == "c") {
+            $screen.innerText = " "
+            return true
+        }
+
+        $screen.append(value)
+    })
+})
